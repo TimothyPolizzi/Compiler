1c51927889793eb15adcb8ed04b0869e74f0ce0b
Tim <timpolizzi2@gmail.com>
2019-09-18 22:18:18 -0400

    Test Code
    As all good things exist, so does test code. And I wanted some nice, error prone test code. I definetly forgot some cases but I just want to break my code more.

--- src/Lexer/LexerTester.java
+++ src/Lexer/LexerTester.java
@@ -6,8 +6,16 @@ import Lexer.Lexer;
 public class LexerTester {
 
   public static void main(String[] args) {
-    Lexer lex = new Lexer("Tim is here");
-    System.out.print(lex.checkIfLegal("}"));
+    String text = "Tim is here$";
+    String quote = "\"I enjoy food\"$";
+    String quoteNoEnd = "\"Big Yoshi$";
+    String integer = "123456790$";
+    String decimal = "123.456$";
+    String keywords = "int a = 123; for(int i = 0; i < 10; i++) { print(\"hi\");};$";
+    String javaKeywords = "int abc = 123; for(int incr = 0; incr < 10; incr++) { if( a != b) { System.out.print(\"hi\")};}$";
+    String noSpaces = "abcint123printwhile";
+
+    Lexer lex = new Lexer(noSpaces);
   }
 
 }
