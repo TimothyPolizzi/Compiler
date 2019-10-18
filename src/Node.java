4240c384cdbaa7e0cba62dcc5918bacb4865a842
Tim <timpolizzi2@gmail.com>
2019-10-17 23:35:58 -0400

    The Tree
    A tree that is as generic as I could make it. Its gonna make my brain melt from recursion but its gonna be a happy goopy mess if I can get this thing done.

---
+++ src/Node.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+  private Object val;
+  private List children;
+  private Node parent;
+
+  /**
+   * Generates a new node that contains a Token with a given parent.
+   *
+   * @param parent The parent Node of the node.
+   * @param val The Token value of the node.
+   */
+  public Node(Node parent, Object val) {
+    this(val);
+    this.parent = parent;
+  }
+
+  /**
+   * Generates a new node that contains a value.
+   *
+   * @param val The value of the node.
+   */
+  public Node(Object val) {
+    children = new ArrayList();
+    this.val = val;
+  }
+
+  /**
+   * Sets the Node parent of the node.
+   *
+   * @param parent The Node parent of the node.
+   */
+  public void setParent(Node parent) {
+    this.parent = parent;
+  }
+
+  /**
+   * Gets the parent Node of this node.
+   *
+   * @return The Node parent node of this node.
+   */
+  public Node getParent() {
+    return parent;
+  }
+
+  /**
+   * Gets the value stored in the node.
+   *
+   * @return the value stored in the node.
+   */
+  public Object getVal() {
+    return val;
+  }
+
+  /**
+   * Appends a child Node to the List of children.
+   *
+   * @param child the Node to be added to the list of children.
+   */
+  public void addChild(Node child) {
+    children.add(child);
+  }
+
+  /**
+   * Gets the List of children of the current Node.
+   *
+   * @return the List of children.
+   */
+  public List getChildren() {
+    return children;
+  }
+}
---
+++ src/SyntaxTree.java
@@ -0,0 +1,95 @@
+import java.util.List;
+
+/**
+ * A tree that can only add direct children. To utilize this properly, it will have a tree created
+ * for each production and add them back up together to make a fully functional tree.
+ */
+public class SyntaxTree {
+
+  private Node root;
+  private int size;
+
+  /**
+   * Generates a new SyntaxTree with a given String as its root.
+   *
+   * @param str the String to be the root of the SyntaxTree.
+   */
+  public SyntaxTree(String str) {
+    root = new Node(str);
+  }
+
+  /**
+   * Gets the root of the SyntaxTree.
+   *
+   * @return the Node root of the SyntaxTree.
+   */
+  public Node getRoot() {
+    return root;
+  }
+
+  /**
+   * Gets the current size of the SyntaxTree.
+   *
+   * @return the int value of the size of the SyntaxTree.
+   */
+  public int getSize() {
+    return size;
+  }
+
+  /**
+   * Adds a new Node containing the specified String to the SyntaxTree.
+   *
+   * @param root the String the new Node is to contain.
+   */
+  public void add(String root) {
+    this.root.addChild(new Node(this.root, root));
+    size++;
+  }
+
+  /**
+   * Adds a preexisting tree to the current tree's children.
+   *
+   * @param tree The SyntaxTree to be added to tree.
+   */
+  public void add(SyntaxTree tree) {
+    Node newChild = tree.getRoot();
+
+    root.addChild(newChild);
+    newChild.setParent(root);
+
+    size += tree.getSize();
+  }
+
+  /**
+   * Gets a string that contains the whole SyntaxTree in a String format.
+   *
+   * @return a String containing the SyntaxTree.
+   */
+  public String depthFirstTraversal() {
+    List childList = root.getChildren();
+    int depth = 0;
+    String toReturn = root.getVal().toString() + "\n";
+
+    toReturn += dft(toReturn, childList, depth);
+
+    return toReturn;
+  }
+
+  private String dft(String toReturn, List children, int depth) {
+    depth++;
+    for (Object child : children) {
+      if (child instanceof List) {
+        toReturn += dft(toReturn, (List) child, depth);
+      } else {
+        int j = 0;
+        while (j < depth) {
+          toReturn += "-";
+          j++;
+        }
+        toReturn += child + "\n";
+      }
+    }
+
+    return toReturn;
+  }
+}
